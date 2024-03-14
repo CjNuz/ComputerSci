c0aabbe693c091f213a18cfc8683b2f0f69b1bf1
Christian Arao <23399@students.mrgs.school.nz>
2024-03-15 12:19:26 +1300

    Practice Code

---
+++ GUI/quiz.py
@@ -0,0 +1,41 @@
+from tkinter import *
+
+names_list = []
+
+class QuizStarter:
+    def __init__(self, parent):
+        background_color = "OldLace"
+        # frame set up
+        self.quiz_frame = Frame(parent, bg=background_color, padx=100, pady=100)
+        self.quiz_frame.grid()
+
+        # Label widget for our heading
+        self.heading_label = Label(self.quiz_frame, text="NZ Road Rules", font=("Tw Cen Mt", "18", "bold"),
+                                    bg=background_color)
+        self.heading_label.grid(row=0)
+
+        # Label for user name prompt
+        self.user_label = Label(self.quiz_frame, text="Please enter your name below", font=("Tw Cen MT", "16"),
+                                bg=background_color)
+        self.user_label.grid(row=1, pady=20)
+
+        # users input is taken by an Entry widget
+        self.entry_box = Entry(self.quiz_frame)
+        self.entry_box.grid(row=2, pady=20)
+
+        # continue button
+        self.continue_button = Button(self.quiz_frame, text="Continue", bg="pink", command=self.name_collection)
+        self.continue_button.grid(row=3, pady=20)
+
+    def name_collection(self):
+        name = self.entry_box.get()
+        names_list.append(name)
+        print(names_list)
+        self.quiz_frame.destroy()
+
+# ---STARTING POINT OF THE PROGRAM---#
+if __name__ == "__main__":
+    root = Tk()
+    root.title("NZ Road Rules Quiz")
+    quizStarter_object = QuizStarter(root)
+    root.mainloop()  # so it doesn't disappear
--- GUI/studentclass.py
+++ GUI/studentclass.py
@@ -3,7 +3,7 @@ from tkinter import *
 class Student ():
     
     def __init__(self, name):
-        self.first_name = "Tushan"
+        self.first_name = name
 
     def display_name(self):
         print(self.first_name)
@@ -15,18 +15,28 @@ class Student ():
         return self.grade
     
 def show_grade():
-    grade_label.config(text=csc_level_2[1].get.grade())
-    pass
-
+    grade_label.config(text=csc_level_2[0].get_grade())
 
 csc_level_2 = []
 
 csc_level_2.append(Student("Shauryya"))
 csc_level_2[0].set_grade("Merit")
-
+csc_level_2.append(Student("Thusan"))
+csc_level_2[0].set_grade("Not Achieve")
 
 window =Tk()
+window.title("Students Grade")
 window.geometry("500x500")
+
+students_listbox = Listbox(window)
+students_listbox.pack()
+students_listbox.insert(1, "Shauryya")
+students_listbox.insert(2, "Thusan")
+
+#student_list = ["Achive", "Merit", "Excellence"]
+
+#for item in student_list:
+    #students_listbox.insert(END, item)
 
 grade_label = Label()
 grade_label.pack()
